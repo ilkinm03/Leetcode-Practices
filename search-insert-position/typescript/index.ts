5595e3b636a0bf794f0bfe63a129f9dc46f0cd2d
ilkinm03 <ilkinm2003@gmail.com>
2023-01-21 18:44:52 +0400

    search insert position solution for typescript

--- search-insert-position/typescript/index.ts
+++ search-insert-position/typescript/index.ts
@@ -9,3 +9,11 @@ function searchInsert(nums: number[], target: number): number {
     }
     return right + 1;
 }
+
+const case1 = searchInsert([1, 3, 5, 6], 5);
+const case2 = searchInsert([1, 3, 5, 6], 2);
+const case3 = searchInsert([1, 3, 5, 6], 7);
+
+console.log(case1);
+console.log(case2);
+console.log(case3);
